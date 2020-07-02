f65dea6ac22933ccf12baded1759ee055966c4b5
sirito97 <66971290+sirito97@users.noreply.github.com>
2020-07-02 14:34:37 +0200

    Body Update

--- Simple snake game.cpp
+++ Simple snake game.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+bool game_over;
+//board
+const int width = 20;
+const int height = 20;
+//snake head
+int x, y;
+//fruit
+int fruit_x, fruit_y;
+//directions
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+//other
+int score;
+void setup()
+{
+    game_over = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruit_x = rand() % width;
+    fruit_y = rand() % height;
+    score = 0;
+}
+void draw()
+{
+    system("cls");
+   
+}
+void input()
+{
 
+}
+void logic()
+{
+
+}
 int main()
 {
-    std::cout << "Hello World!\n";
-    //hello
+    setup();
+    while(!game_over)
+    {
+        draw();
+        input();
+        logic();
+    }
 }
 
